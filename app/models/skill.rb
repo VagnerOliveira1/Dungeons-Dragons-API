311a15f22631c872e3c521200e7e0cb79df77e05
Vagner <ti.vagner@gmail.com>
2019-11-16 14:36:39 -0300

    Resolve validação de skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,9 @@
 class Skill < ApplicationRecord
     
     belongs_to :character, dependent: :destroy
+    validates :name, presence: true, length: {maximum: 25}
+    validates_inclusion_of :ability, in: [ "strength","dexterity", "constitution","inteligence","wisdom" ,"charisma"], presence: true
+
 
     def score
     end
